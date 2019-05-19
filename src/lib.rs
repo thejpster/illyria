9acb3274b950f136f81dab2c54599510bdeeb429
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2019-05-19 20:21:00 +0100

    Transmission basically implemented.
    ) Performs COBS encoding.
    ) Adds X25 checksum.
    ) Adds IFRAME marker.

--- src/lib.rs
+++ src/lib.rs
@@ -16,13 +16,56 @@ where
 
 enum State {
     Idle,
-    Sending(usize, usize),
+    SendingInitialZero { len: usize },
+    Sending { sent: usize, len: usize },
+}
+
+#[derive(Debug, Copy, Clone)]
+struct Checksum(u16);
+
+impl Checksum {
+    fn generate(data: &[u8]) -> Checksum {
+        let result = Checksum(crc::crc16::checksum_x25(data));
+        println!("Checksum of {:?} is {}/0x{:04x}", data, result.0, result.0);
+        result
+    }
+
+    fn validate(self, data: &[u8]) -> bool {
+        crc::crc16::checksum_x25(data) == self.0
+    }
+
+    fn first_byte(self) -> u8 {
+        (self.0 >> 8) as u8
+    }
+
+    fn second_byte(self) -> u8 {
+        self.0 as u8
+    }
 }
 
 impl<T> Illyria<T>
 where
     T: embedded_hal::serial::Write<u8>,
 {
+    const COBS_START_IDX: usize = 0;
+    const FRAME_TYPE_IDX: usize = 1;
+    const PAYLOAD_LENGTH_IDX: usize = 2;
+    const DATA_IDX: usize = 3;
+
+    /// We checksum the payload length, plus 2 bytes (the frame type and the
+    /// length byte)
+    const CHECKSUM_OVERHEAD: usize = 2;
+
+    /// Frame overhead comprises the checksum overhead, plus two bytes of checksum.
+    const FRAME_OVERHEAD: usize = Self::CHECKSUM_OVERHEAD + 2;
+
+    /// Slice overhead is the frame overhead, plus the COBS byte
+    const COBS_OVERHEAD: usize = Self::FRAME_OVERHEAD + 1;
+
+    const IFRAME: u8 = 1;
+    const ACK: u8 = 2;
+    const NACK: u8 = 3;
+
     pub fn new(transport: T) -> Illyria<T> {
         Illyria {
             transport,
@@ -31,18 +74,62 @@ where
         }
     }
 
+    pub fn space(&self) -> usize {
+        self.buffer.len() - Self::COBS_OVERHEAD
+    }
+
     pub fn send<M>(&mut self, message: &M) -> Result<(), ()>
     where
         M: serde::ser::Serialize,
     {
         let _err = self.transport.flush();
-        match postcard::to_slice(message, &mut self.buffer) {
-            Ok(buf) => {
-                self.state = State::Sending(0, buf.len());
-                Ok(())
+        match postcard::to_slice(message, &mut self.buffer[Self::DATA_IDX..]).map(|buf| buf.len()) {
+            Ok(payload_len) => {
+                if payload_len <= self.space() {
+                    // Build a complete frame
+                    self.buffer[Self::FRAME_TYPE_IDX] = Self::IFRAME;
+                    self.buffer[Self::PAYLOAD_LENGTH_IDX] = payload_len as u8;
+                    let checksum = Checksum::generate(
+                        &self.buffer[Self::FRAME_TYPE_IDX
+                            ..Self::FRAME_TYPE_IDX + payload_len + Self::CHECKSUM_OVERHEAD],
+                    );
+                    self.buffer[Self::DATA_IDX + payload_len] = checksum.first_byte();
+                    self.buffer[Self::DATA_IDX + payload_len + 1] = checksum.second_byte();
+                    let slice_length = self.cobs_encode(payload_len + Self::FRAME_OVERHEAD);
+                    self.state = State::SendingInitialZero { len: slice_length };
+                    Ok(())
+                } else {
+                    // Message doesn't fit in the buffer when overheads are added
+                    Err(())
+                }
             }
             Err(_error) => Err(()),
         }
+    }
+
+    /// This only works for payloads under 254 bytes in length - we can't handle
+    /// the insertion of extra zeroes required when the buffer is longer than that.
+    fn cobs_encode(&mut self, len: usize) -> usize {
+        // Need to fill in our first byte as the offset to the first zero then
+        // replace each zero with the offset to the next zero.
+        let mut last_idx = Self::COBS_START_IDX;
+        for i in (Self::COBS_START_IDX + 1)..len {
+            let b = self.buffer[i];
+            if b == 0 {
+                let gap_to_zero = i - last_idx;
+                self.buffer[last_idx] = gap_to_zero as u8;
+                println!("Setting idx {} to {}", last_idx, gap_to_zero);
+                last_idx = i;
+            }
+        }
+        let gap_to_zero = 1 + len - last_idx;
+        self.buffer[last_idx] = gap_to_zero as u8;
+        println!("Setting idx {} to {}", last_idx, gap_to_zero);
+        len + 1
+    }
+
+    pub fn reset(&mut self) {
+        self.state = State::Idle;
     }
 
     pub fn poll(&mut self) -> nb::Result<(), ()> {
@@ -51,24 +138,37 @@ where
                 // Do nothing
                 Ok(())
             }
-            State::Sending(n, len) => {
-                match self.transport.write(self.buffer[n]) {
-                    Ok(_) => {
-                        let new_n = n + 1;
-                        if new_n == len {
-                            self.state = State::Idle;
-                        } else {
-                            self.state = State::Sending(n + 1, len);
-                        }
-                        Ok(())
-                    }
-                    Err(nb::Error::WouldBlock) => {
-                        // Try again later
-                        Err(nb::Error::WouldBlock)
-                    }
-                    Err(nb::Error::Other(_e)) => Err(nb::Error::Other(())),
-                }
+            State::SendingInitialZero { len } => {
+                self.transmit_byte(0x00)?;
+                self.state = State::Sending { sent: 0, len };
+                Ok(())
             }
+            State::Sending { sent, len } => {
+                // Send the complete frame
+                let b = self.buffer[sent];
+                self.transmit_byte(b)?;
+                let new_sent = sent + 1;
+                if new_sent == len {
+                    self.state = State::Idle;
+                } else {
+                    self.state = State::Sending {
+                        sent: new_sent,
+                        len,
+                    };
+                }
+                Ok(())
+            }
+        }
+    }
+
+    fn transmit_byte(&mut self, byte: u8) -> nb::Result<(), ()> {
+        match self.transport.write(byte) {
+            Ok(_) => Ok(()),
+            Err(nb::Error::WouldBlock) => {
+                // Try again later
+                Err(nb::Error::WouldBlock)
+            }
+            Err(nb::Error::Other(_e)) => Err(nb::Error::Other(())),
         }
     }
 
@@ -98,6 +198,7 @@ mod tests {
         A,
         B(u32),
         C(bool),
+        D([u32; 16]),
     }
 
     impl embedded_hal::serial::Write<u8> for TestTransport {
@@ -115,7 +216,7 @@ mod tests {
     }
 
     #[test]
-    fn encode() {
+    fn encode_a() {
         let t = TestTransport {
             out_buffer: Vec::new(),
         };
@@ -126,18 +227,74 @@ mod tests {
         for _ in 0..100 {
             illyria.poll().unwrap();
         }
-        illyria.access_transport().check(&[0]);
+        illyria.access_transport().check(&[
+            0,                                // COBS delimiter
+            3,                                // Gap to next zero
+            Illyria::<TestTransport>::IFRAME, // Frame type
+            1,                                // Length
+            3,                                // Payload 0
+            0x85,                             // Checksum 0
+            0xC8,                             // Checksum 1
+        ]);
+    }
 
-        illyria.send(&Message::B(0)).unwrap();
+    #[test]
+    fn encode_b() {
+        let t = TestTransport {
+            out_buffer: Vec::new(),
+        };
+
+        let mut illyria = Illyria::new(t);
+
+        illyria.send(&Message::B(0x06070809)).unwrap();
         for _ in 0..100 {
             illyria.poll().unwrap();
         }
-        illyria.access_transport().check(&[1, 0, 0, 0, 0]);
+        illyria.access_transport().check(&[
+            0,                                // COBS delimiter
+            10,                               // Gap to next zero
+            Illyria::<TestTransport>::IFRAME, // Frame type
+            5,                                // Length
+            1,                                // Payload 0
+            9,                                // Payload 1
+            8,                                // Payload 2
+            7,                                // Payload 3
+            6,                                // Payload 4
+            0x1B,                             // Checksum 0
+            0xF9,                             // Checksum 1
+        ]);
+    }
+
+    #[test]
+    fn encode_c() {
+        let t = TestTransport {
+            out_buffer: Vec::new(),
+        };
+
+        let mut illyria = Illyria::new(t);
 
         illyria.send(&Message::C(true)).unwrap();
         for _ in 0..100 {
             illyria.poll().unwrap();
         }
-        illyria.access_transport().check(&[2, 1]);
+        illyria.access_transport().check(&[
+            0,                                // COBS delimiter
+            7,                                // Gap to next zero
+            Illyria::<TestTransport>::IFRAME, // Frame type
+            2,                                // Length
+            2,                                // Payload 0
+            1,                                // Payload 1
+            0x77,                             // Checksum 0
+            0xE4,                             // Checksum 1
+        ]);
+    }
+
+    #[test]
+    fn encode_too_big() {
+        let t = TestTransport {
+            out_buffer: Vec::new(),
+        };
+        let mut illyria = Illyria::new(t);
+        assert!(illyria.send(&Message::D([0; 16])).is_err());
     }
 }
